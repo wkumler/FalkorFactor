8fbe6f2d587cf488e4baf58679f97a6c3bf40aa1
wkumler <wkumler@berkeley.edu>
2019-09-26 16:42:41 -0700

    Added MS2 scraper :whale:

--- scrapeMetlin.R
+++ scrapeMetlin.R
@@ -24,14 +24,12 @@ amino_masses <- "http://www.matrixscience.com/help/aa_help.html" %>%
 # getMetlin code ----
 getMetlin <- function(cmpd_mz, ppm=2.5){
   mz_range <- mzr(cmpd_mz, ppm)
-  metlin_url <- paste0("https://metlin.scripps.edu/advanced_search_result.php?",
+  metlin_data <- paste0("https://metlin.scripps.edu/advanced_search_result.php?",
                        "molid=&mass_min=", min(mz_range),
                        "&mass_max=", max(mz_range), "&Amino",
-                       "Acid=add&drug=add&toxinEPA=add&keggIDFilter=add")
-  metlin_raw <- GET(metlin_url)
-  stop_for_status(metlin_raw)
-  
-  metlin_data <- metlin_raw %>%
+                       "Acid=add&drug=add&toxinEPA=add&keggIDFilter=add") %>%
+    GET() %>%
+    stop_for_status() %>%
     content(encoding = "UTF-8") %>%
     xml_find_all(xpath = "//tbody")
   
@@ -62,7 +60,42 @@ sample_data <- getMetlin(117.078979)
 
 metlin_data <- lapply(amino_masses[1:3], getMetlin)
 
-
-
-# getHMDB code ----
-
+getMetlinMS2 <- function(cmpd_id){
+  metlin_data <- paste0("https://metlin.scripps.edu/showChart.php?molid=", cmpd_id,
+                        "&etype=experimental") %>%
+    GET() %>%
+    stop_for_status()
+  ms2_raw <- metlin_data %>%
+    content(encoding = "UTF-8") %>%
+    xml_find_all(xpath = '/html/head/script[5]') %>%
+    xml_text() %>%
+    gsub(pattern = ".*series: \\[", replacement = "") %>%
+    gsub(pattern = " ]}]\n                }); \n\n            });\n        ",
+         replacement = "") %>%
+    gsub(pattern = "&nbsp;", replacement = "") %>%
+    strsplit(split = "\\{name: ") %>%
+    unlist() %>%
+    `[`(-1) %>%
+    lapply(strsplit, split=",data:\\[")
+  
+  if(!length(ms2_raw)){stop("No MS2 data found. Are you sure Metlin has this data?")}
+  
+  ms2_titles <- sapply(ms2_raw, `[[`, 1)[1,] #Not sure why this works but OK
+  ms2_polarities <- ifelse(grepl(ms2_titles, pattern = "(\\+)"), "Pos", "Neg")
+  ms2_voltages <- as.numeric(unlist(regmatches(ms2_titles, m = gregexpr("\\d+\\.?\\d*", ms2_titles))))
+  ms2_adducts <- unlist(regmatches(ms2_titles, m = gregexpr("\\[M.*\\]", ms2_titles)))
+  
+  ms2_list <- sapply(ms2_raw, `[[`, 1)[2,] %>%
+    gsub(pattern = " ]},", replacement = "") %>%
+    lapply(function(x){
+      wo1 <- substring(x, 2)
+      wo2 <- substring(wo1, 1, nchar(wo1))
+      ms2_list <- unlist(strsplit(wo2, "},\\{"))
+      ms2_list <- lapply(ms2_list, function(x)
+        as.numeric(unlist(regmatches(x, m = gregexpr("\\d+\\.?\\d*", x)))))
+      ms2_df <- as.data.frame(do.call(rbind, ms2_list))
+      names(ms2_df) <- c("mass", "rel_intensity")
+      return(ms2_df)
+    })
+  v <- mapply(FUN= list, ms2_polarities, ms2_adducts, ms2_voltages)
+}

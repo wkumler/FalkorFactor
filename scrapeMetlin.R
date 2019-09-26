cfa7e6faef7213f5e859794a14d3877ae464b488
wkumler <wkumler@berkeley.edu>
2019-09-25 19:23:34 -0700

    Switched to GET() instead of read_html() to attempt referer and user agent spoofing

--- scrapeMetlin.R
+++ scrapeMetlin.R
@@ -9,7 +9,8 @@ pm <- function(x, d){c(x-d, x+d)}
 mzr <- function(mz, ppm=2.5){round(pm(mz, mz*ppm/1000000), digits = 5)}
 
 amino_masses <- "http://www.matrixscience.com/help/aa_help.html" %>%
-  read_html() %>%
+  GET() %>%
+  content() %>%
   xml_find_all(xpath="//td") %>%
   xml_text() %>%
   matrix(ncol=6, byrow=T) %>%
@@ -27,8 +28,11 @@ getMetlin <- function(cmpd_mz){
                        "molid=&mass_min=", min(mz_range),
                        "&mass_max=", max(mz_range), "&Amino",
                        "Acid=add&drug=add&toxinEPA=add&keggIDFilter=add")
-  metlin_data <- metlin_url %>%
-    read_html() %>%
+  metlin_raw <- GET(metlin_url)
+  stop_for_status(metlin_raw)
+  
+  metlin_data <- metlin_raw %>%
+    content() %>%
     xml_find_all(xpath = "//tbody") %>%
     xml_find_all(xpath="//td") %>%
     xml_text() %>%
@@ -48,3 +52,13 @@ sample_data <- getMetlin(135.054495)
 
 metlin_data <- lapply(amino_masses, getMetlin)
 
+
+
+
+test_url <- 'https://httpbin.org/user-agent'
+metlin_url <- paste0("https://metlin.scripps.edu/advanced_search_result.php?",
+                     "molid=&mass_min=", min(mz_range),
+                     "&mass_max=", max(mz_range), "&Amino",
+                     "Acid=add&drug=add&toxinEPA=add&keggIDFilter=add")
+GET(url = test_url, add_headers('User-Agent'=user_agent))
+GET(url = metlin_url, add_headers("User-Agent"=user_agent))

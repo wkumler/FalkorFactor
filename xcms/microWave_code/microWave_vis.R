a8110b0ff5386fbeba5907cee0f0de9983786823
wkumler <wkumler@berkeley.edu>
2019-10-24 15:05:14 -0700

    Added isoCheck functionality and cleaned up peak visualization

--- xcms/microWave_code/microWave_vis.R
+++ xcms/microWave_code/microWave_vis.R
@@ -1,5 +1,8 @@
 
+library(dplyr)
+
 source("xcms/microWave_code/microWave_functions.R")
+load("xcms/microWave_code/temp_eic_list")
 peak_df <- read.csv("xcms/microWave_code/temp_peak_df.csv")
 
 peak_df <- mutate(peak_df, qscore=Peak_SNR*Peak_gauss_fit^4*log10(Peak_area))
@@ -21,7 +24,7 @@ for(i in peak_df$Peak_id){
   readline(prompt = "Continue?")
 }
 
-peak_df_best <- filter(peak_df, qscore>5)
+peak_df_best <- filter(peak_df, qscore>3)
 
 pdf(file = "xcms/microWave_code/TempPeakPlot.pdf")
 ylimits <- c(min(peak_df_best$Peak_mz), max(peak_df_best$Peak_mz))
@@ -29,11 +32,11 @@ xlimits <- c(min(peak_df_best$Peak_start_time), max(peak_df_best$Peak_end_time))
 plot(1, ylim=ylimits, xlim=xlimits)
 factored_peakareas <- factor(round(log2(peak_df_best$Peak_area)))
 peak_shades <- gray.colors(length(unique(factored_peakareas)), start = 0, end = 1)[factored_peakareas]
-peak_shades <- hcl.colors(length(unique(factored_peakareas)))[factored_peakareas]
+peak_shades <- hcl.colors(length(unique(factored_peakareas)), rev = T)[factored_peakareas]
 for(i in seq_len(nrow(peak_df_best))){
   segments(x0=peak_df_best[i, "Peak_start_time"], x1=peak_df_best[i, "Peak_end_time"],
            y0=peak_df_best[i, "Peak_mz"], y1=peak_df_best[i, "Peak_mz"],
-           col = peak_shades[i])
+           col = peak_shades[i], lwd=floor(log(peak_df_best[i, "qscore"])))
   text(x = mean(c(peak_df_best[i, "Peak_start_time"], peak_df_best[i, "Peak_end_time"])),
        y = peak_df_best[i, "Peak_mz"]+0.2, labels = peak_df_best[i, "Peak_id"], 
        cex = 0.5, col = peak_shades[i])
@@ -44,8 +47,12 @@ dev.off()
 # Isotopes between 100 and 120 Da
 isoCheck(peak_df, eic_list, "1.1.1", "3.4.1")
 isoCheck(peak_df, eic_list, "12.1.4", "116.4.1")
-isoCheck(peak_df, eic_list, "17.1.1", "115.1.1")
 isoCheck(peak_df, eic_list, "5.1.2", "57.1.1")
 isoCheck(peak_df, eic_list, "43.4.1", "60.1.1")
 isoCheck(peak_df, eic_list, "7.1.7", "81.1.1")
 isoCheck(peak_df, eic_list, "7.1.2", "81.2.1")
+
+
+isoCheck(peak_df, eic_list, "5.1.1", "40.2.1")
+isoCheck(peak_df, eic_list, "37.1.2", "15.1.2")
+isoCheck(peak_df, eic_list, "17.1.1", "115.1.1")

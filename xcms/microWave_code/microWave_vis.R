e19eb7582b581f129ef9eafdca478c63942180cd
wkumler <wkumler@berkeley.edu>
2019-10-25 11:04:33 -0700

    Moved around visualization code

--- xcms/microWave_code/microWave_vis.R
+++ xcms/microWave_code/microWave_vis.R
@@ -3,9 +3,8 @@
 library(dplyr)
 
 source("xcms/microWave_code/microWave_functions.R")
-load("xcms/microWave_code/temp_eic_list")
-peak_df <- read.csv("xcms/microWave_code/temp_peak_df.csv", stringsAsFactors = F)
-peak_df <- mutate(peak_df, qscore=Peak_SNR*Peak_gauss_fit^4*log10(Peak_area))
+
+
 peak_df <- arrange(peak_df, desc(qscore))
 
 #Look at all the pretty peaks! ----
@@ -26,6 +25,20 @@ peakCheck(eic_list, peak_df, "1.1.1")
 # }
 
 
+
+# Visualize the isotopes
+isotope_df <- filter(peak_df, !is.na(Isotopes)) #%>% select("Peak_id", "Isotopes")
+isoCheck(peak_df = peak_df, eic_list = eic_list, 
+         peak_id_1 = isotope_df$Peak_id[1], 
+         peak_id_2 = isotope_df$Isotopes[[1]]$`1`$Peak_id)
+# for(i in seq_len(nrow(isotope_df))){
+#   isoCheck(peak_df = peak_df, eic_list = eic_list,
+#            peak_id_1 = isotope_df$Peak_id[i],
+#            peak_id_2 = isotope_df$Isotopes[[i]]$`1`$Peak_id)
+# }
+
+
+
 # Decide which ones are worth keeping (quality score>1)
 # and which ones are worth analyzing (quality score > 5)
 peak_df <- filter(peak_df, qscore>1)
@@ -37,15 +50,3 @@ renderPeakOverview(peak_df_best)
 # pdf(file = "xcms/microWave_code/TempPeakPlot.pdf")
 # renderPeakOverview(peak_df_best)
 # dev.off()
-
-
-# Visualize the isotopes
-isotope_df <- filter(peak_df, !is.na(Isotopes)) #%>% select("Peak_id", "Isotopes")
-isoCheck(peak_df = peak_df, eic_list = eic_list, 
-         peak_id_1 = isotope_df$Peak_id[1], 
-         peak_id_2 = isotope_df$Isotopes[[1]]$`1`$Peak_id)
-for(i in seq_len(nrow(isotope_df))){
-  isoCheck(peak_df = peak_df, eic_list = eic_list,
-           peak_id_1 = isotope_df$Peak_id[i],
-           peak_id_2 = isotope_df$Isotopes[[i]]$`1`$Peak_id)
-}

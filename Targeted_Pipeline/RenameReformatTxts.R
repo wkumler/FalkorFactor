78bef6ac95f375b0cdf5af811146c19906c78681
wkumler <wkumler@uw.edu>
2020-02-12 13:37:09 -0800

    Updating filenames and types to match Regina scheme

---
+++ Targeted_Pipeline/RenameReformatTxts.R
@@ -0,0 +1,16 @@
+# Code to convert MSDIAL's default output of 
+# .txt files to .csv files with nice names
+
+filenames <- list.files("data_raw", pattern = "\\.txt", full.names = TRUE)
+
+filedata <- lapply(X = filenames, FUN = read.csv, sep = "\t")
+new_filenames <- gsub(pattern = "txt", replacement = "csv", filenames)
+new_filenames <- gsub(pattern = "_0_[[:digit:]]*", replacement = "_HILICPos", new_filenames)
+new_filenames <- gsub(pattern = "_1_[[:digit:]]*", replacement = "_HILICNeg", new_filenames)
+names(filedata) <- new_filenames
+
+sapply(seq_along(filedata), function(x){
+  write.csv(filedata[[x]], file = names(filedata)[x])
+})
+
+file.remove(filenames)

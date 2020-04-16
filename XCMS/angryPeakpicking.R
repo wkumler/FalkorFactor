e3c64907305102b618e4296c8da83cb962ffbbf9
wkumler <wkumler@uw.edu>
2020-04-15 22:02:40 -0700

    Adding metadata and tuning peakpicking params

--- XCMS/angryPeakpicking.R
+++ XCMS/angryPeakpicking.R
@@ -1,26 +1,62 @@
 # Angry peakpicking
+library(xcms)
+library(tidyverse)
+library(data.table)
 
-xdata_cor <- readRDS(file = "XCMS/temp_data/current_xdata_cor.rds")
-all_data <- as.data.table(readRDS(file = "MS1_data_frame"))
-peakdf_qscored <- as.matrix(read.csv(file = "XCMS/temp_data/peakdf_qscored.csv"))
+register(BPPARAM = SerialParam())
 
+ms_files <- "mzMLs" %>%
+  list.files(pattern = ".mzML", full.names = TRUE) %>%
+  normalizePath() %>%
+  `[`(!grepl("Fullneg|Fullpos|QC-KM1906", x = .))
+metadata <- data.frame(
+  fileid=basename(ms_files),
+  sample_group=c("Blank", "Pooled", "Sample", "Std")[c(1, rep(2, 6), rep(3, 24), rep(4, 10))],
+  depth=c("Blank", "Pooled", "DCM", "25m", "Std")[c(1, rep(2, 6), rep(c(rep(3, 3), rep(4, 3)), 4), rep(5, 10))],
+  spindir=c("Blank", "Pooled", "Cyclone", "Anticyclone", "Std")[c(1, rep(2, 6), rep(3, 12), rep(4, 12), rep(5, 10))],
+  time=c("Blank", "Pooled", "Morning", "Afternoon", "Std")[c(1, rep(2, 6), rep(c(rep(3, 6), rep(4, 6)), 2), rep(5, 10))]
+)
+raw_data <- readMSData(files = ms_files, pdata = metadata, 
+                       mode = "onDisk", verbose = TRUE)
 
+all_data <- as.data.table(readRDS(file = "XCMS/MS1_data_frame"))
 
-xdata_cor %>% featureDefinitions() %>% as.data.frame() %>% 
-  filter(mzmed%between%pmppm(90.05550455, 5))
+given_mass <- 143.082053
+given_mass <- 118.0865
+given_rt <- 450
+given_rt <- 350
 
-chromPeaks(xdata_cor) %>% as.data.frame() %>% 
-  filter(mz%between%pmppm(90.05550455, 10)) %>%
-  arrange(rt)
+rtr <- (given_rt+c(-100, 100))
+mzr <- pmppm(given_mass, 10)
+group_colors <- c("red", "black", "steelblue", NA)
+chr_raw <- xcms::chromatogram(raw_data, mz = mzr, rt = rtr)
+plot(chr_raw, col = group_colors[chr_raw$sample_group])
 
-peakdf_qscored %>% as.data.frame() %>% 
-  filter(mz%between%pmppm(90.05550455, 10)) %>%
-  ggplot() + geom_rect(aes(xmin=rtmin, xmax=rtmax, ymin=mzmin, 
-                           ymax=mzmax, fill=log10(maxo)), color="black") +
-  facet_wrap(~sample) + scale_fill_viridis_c() +
-  xlim(c(500, 650))
+cwp <- CentWaveParam(ppm = 5, peakwidth = c(10, 20), 
+                     snthresh = 0, prefilter = c(0,0), 
+                     integrate = 2, mzCenterFun = "wMean", 
+                     mzdiff = 0.0001, fitgauss = FALSE, 
+                     noise = 0, firstBaselineCheck = FALSE, 
+                     extendLengthMSW = TRUE)
+xchr <- findChromPeaks(chr_raw, param = cwp)
+chrom_peaks <- as.data.frame(chromPeaks(xchr)) %>% 
+  mutate(fileid=column) %>% filter(fileid<26)
 
-eics <- all_data[mz%between%pmppm(90.05550455, 10)&
-                   rt%between%c(500,650)]
-ggplot(eics) + geom_line(aes(x=rt, y=int)) + 
+eics <- all_data[mz%between%pmppm(given_mass, 5)&
+                   rt%between%(given_rt+c(-100, 100))]
+ggplot() + 
+  geom_line(data = eics, aes(x=rt, y=int, group=fileid)) + 
+  geom_vline(data = chrom_peaks, aes(xintercept=rtmin+1), col="red") +
+  geom_vline(data = chrom_peaks, aes(xintercept=rtmax-1), col="red") #+
   facet_wrap(~fileid, scales = "free_y")
+chrom_peaks <- mutate(chrom_peaks, mzmin=min(eics$mz), mzmax=max(eics$mz))
+
+lapply(seq_len(nrow(chrom_peaks)), FUN=xcmsQscoreCalculator, 
+       xcms_peakdf=chrom_peaks, file_data_table=eics, 
+       qscoreCalculator=qscoreCalculator) %>%
+  do.call(what = rbind) %>%
+  cbind(chrom_peaks, .) %>%
+  filter(fileid==5) %>% arrange(rt)
+
+head(xcms::rtime(xchr[[5]]))
+head(eics[fileid==5])

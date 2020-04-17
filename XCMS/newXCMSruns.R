481a3d83394b8c5a8746a59fcc2a8402483799cc
wkumler <wkumler@uw.edu>
2020-04-17 00:12:15 -0700

    Created less rebellious XCMS/CAMERA file

---
+++ XCMS/newXCMSruns.R
@@ -0,0 +1,177 @@
+# XCMS, but with CAMERA this time
+
+
+# Functions ----
+pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
+grabSingleFileData <- function(filename){
+  msdata <- mzR:::openMSfile(filename)
+  fullhd <- mzR::header(msdata)
+  spectra_list <- lapply(seq_len(nrow(fullhd)), function(x){
+    given_peaks <- mzR::peaks(msdata, x)
+    rtime <- fullhd[x, "retentionTime"]
+    return(cbind(rtime, given_peaks))
+  })
+  all_data <- `names<-`(as.data.frame(do.call(rbind, spectra_list)), 
+                        c("rt", "mz", "int"))
+  return(all_data)
+}
+grabSingleFileMS2 <- function(filename){
+  msdata <- mzR::openMSfile(filename)
+  fullhd <- mzR::header(msdata)
+  ms2rows <- seq_len(nrow(fullhd))[fullhd$msLevel>1]
+  spectra_list <- lapply(ms2rows, function(x){
+    rtime <- fullhd[x, "retentionTime"]
+    premz <- fullhd[x, "precursorMZ"]
+    fragments <- mzR::peaks(msdata, x)
+    return(cbind(rtime, premz, fragments))
+  })
+  all_data <- `names<-`(as.data.frame(do.call(rbind, spectra_list)), 
+                        c("rt", "premz", "fragmz", "int"))
+  return(all_data)
+}
+speedyQscoreCalculator <- function(file_peaks, grabSingleFileData, qscoreCalculator){
+  library(data.table)
+  file_data <- grabSingleFileData(unique(file_peaks$filename))
+  file_data_table <- as.data.table(file_data)
+  peak_splits <- split(file_peaks, ceiling(seq_len(nrow(file_peaks))/100))
+  file_qscores <- lapply(peak_splits, function(i){
+    eic_many <- file_data_table[mz%between%c(min(i$mzmin), max(i$mzmax))]
+    individual_peaks <- split(i, seq_len(nrow(i)))
+    output <- sapply(individual_peaks, function(peak_row){
+      eic <- eic_many[rt%between%c(peak_row$rtmin, peak_row$rtmax) & 
+                        mz%between%c(peak_row$mzmin, peak_row$mzmax)]
+      qscoreCalculator(eic)
+    }, USE.NAMES = FALSE)
+  })
+  file_peaks$sn <- unlist(file_qscores)
+  return(file_peaks)
+}
+qscoreCalculator <- function(eic){
+  #Check for bogus EICs
+  if(nrow(eic)<5){
+    return(0)
+  }
+  #Calculate where each rt would fall on a beta dist (accounts for missed scans)
+  scaled_rts <- (eic$rt-min(eic$rt))/(max(eic$rt)-min(eic$rt))
+  
+  # Create a couple different skews and test fit
+  maybe_skews <- c(2.5,3,4,5) #Add 7 to catch more multipeaks and more noise
+  #Add 2 to catch very slopey peaks and more noise
+  best_skew <- maybe_skews[which.max(sapply(maybe_skews, function(x){
+    cor(dbeta(scaled_rts, shape1 = x, shape2 = 5), eic$int)
+  }))]
+  perf_peak <- dbeta(scaled_rts, shape1 = best_skew, shape2 = 5)
+  peak_cor <- cor(perf_peak, eic$int)
+  
+  
+  #Calculate the normalized residuals
+  residuals <- eic$int/max(eic$int)-perf_peak/max(perf_peak)
+  #Calculate the minimum SD, after normalizing for any shape discrepancy
+  old_res_sd <- sd(residuals)
+  norm_residuals <- diff(residuals)
+  new_res_sd <- sd(norm_residuals)
+  while(new_res_sd<old_res_sd){
+    old_res_sd <- new_res_sd
+    norm_residuals <- diff(residuals)
+    new_res_sd <- sd(residuals)
+  }
+  #Calculate SNR
+  SNR <- (max(eic$int)-min(eic$int))/sd(norm_residuals*max(eic$int))
+  #Return the quality score
+  return(round(SNR*peak_cor^4*log10(max(eic$int))))
+}
+
+
+
+# Setup things ----
+library(xcms)
+library(CAMERA)
+library(tidyverse)
+library(data.table)
+library(pbapply)
+
+ms_files <- "mzMLs" %>%
+  list.files(pattern = ".mzML", full.names = TRUE) %>%
+  normalizePath() %>%
+  `[`(!grepl("Fullneg|Fullpos|QC-KM1906", x = .))
+register(BPPARAM = SnowParam(tasks = length(ms_files), progressbar = TRUE))
+
+metadata <- data.frame(
+  fileid=basename(ms_files),
+  sample_group=c("Blank", "Pooled", "Sample", "Std")[c(1, rep(2, 6), rep(3, 24), rep(4, 10))],
+  depth=c("Blank", "Pooled", "DCM", "25m", "Std")[c(1, rep(2, 6), rep(c(rep(3, 3), rep(4, 3)), 4), rep(5, 10))],
+  spindir=c("Blank", "Pooled", "Cyclone", "Anticyclone", "Std")[c(1, rep(2, 6), rep(3, 12), rep(4, 12), rep(5, 10))],
+  time=c("Blank", "Pooled", "Morning", "Afternoon", "Std")[c(1, rep(2, 6), rep(c(rep(3, 6), rep(4, 6)), 2), rep(5, 10))]
+) %>% new(Class = "NAnnotatedDataFrame")
+
+
+
+# Peakpicking ----
+start_time <- Sys.time()
+raw_data <- readRDS(file = "XCMS/temp_data/current_raw_data.rds")
+register(BPPARAM = SnowParam(tasks = length(ms_files), progressbar = TRUE))
+cwp <- CentWaveParam(ppm = 2.5, peakwidth = c(15, 15), 
+                     snthresh = 1, prefilter = c(0, 10000), 
+                     integrate = 2, mzCenterFun = "wMean", 
+                     mzdiff = 0.001, fitgauss = FALSE, 
+                     noise = 5000, firstBaselineCheck = FALSE, 
+                     extendLengthMSW = TRUE)
+xdata <- suppressMessages(findChromPeaks(raw_data, param = cwp))
+saveRDS(xdata, file = "XCMS/temp_data/current_xdata.rds")
+print(Sys.time()-start_time)
+# 8 minutes
+
+start_time <- Sys.time()
+xdata <- readRDS(file = "XCMS/temp_data/current_xdata.rds")
+xcms_peakdf <- chromPeaks(xdata) %>%
+  as.data.frame(stringsAsFactors=FALSE) %>%
+  mutate(filename=fileNames(xdata)[.[["sample"]]]) %>%
+  arrange(mz)
+split_xcms_filedata <- split(xcms_peakdf, xcms_peakdf$filename)
+files_qscores <- bplapply(X = split_xcms_filedata, FUN = speedyQscoreCalculator,
+                          grabSingleFileData, qscoreCalculator)
+
+threshold <- 25
+peakdf_qscored <- files_qscores %>%
+  do.call(what = rbind) %>%
+  as.data.frame(stringsAsFactors=FALSE) %>%
+  filter(sn>threshold) %>%
+  select(-filename) %>%
+  arrange(sample, rtmin, rtmax) %>%
+  as.matrix()
+write.csv(peakdf_qscored, file = "XCMS/temp_data/peakdf_qscored.csv", 
+          row.names = FALSE)
+xdata_cleanpeak <- `chromPeaks<-`(xdata, peakdf_qscored)
+saveRDS(xdata_cleanpeak, file = "XCMS/temp_data/clean_xdata.rds")
+print(Sys.time()-start_time)
+# 10 minutes?
+
+
+
+# Plot picked peaks, just for kicks! ----
+given_file <- ms_files[2]
+clean_peakdf <- chromPeaks(xdata_cleanpeak) %>%
+  as.data.frame(stringsAsFactors=FALSE) %>%
+  mutate(filename=fileNames(xdata)[.[["sample"]]]) %>%
+  arrange(sn) %>%
+  filter(filename==given_file)
+file_eic <- as.data.table(grabSingleFileData(given_file))
+pdf(width = 17, height = 11)
+par(mfrow=c(6, 8))
+par(mar=c(2.1, 2.1, 0.1, 0.1))
+apply(clean_peakdf, 1, function(x){
+  eic <- file_eic[rt%between%(as.numeric(c(x["rtmin"], x["rtmax"]))+c(-50, 50))&
+                    mz%between%as.numeric(c(x["mzmin"], x["mzmax"]))]
+  plot(eic$rt, eic$int, type="l", xlab="", ylab="", 
+       ylim=c(0, max(eic$int)*1.2), xlim=c(min(eic$rt)-10, max(eic$rt)+10))
+  abline(v=as.numeric(x["rtmin"]), col="red")
+  abline(v=as.numeric(x["rtmax"]), col="red")
+  legend("topright", legend = x["sn"], bty="n")
+  legend("topleft", legend = substr(x["mz"], 1, 9), bty="n")
+  return(NULL)
+})
+dev.off()
+
+
+
+# Other XCMS things (rtcor, group) ----

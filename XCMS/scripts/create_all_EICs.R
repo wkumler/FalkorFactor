dac68f44c0563282652ba9db2de92d63d9c1d3e1
wkumler <wkumler@uw.edu>
2020-05-14 22:51:06 -0700

    Cleaned up EIC creation code and replicated for all files

--- XCMS/scripts/create_all_EICs.R
+++ XCMS/scripts/create_all_EICs.R
@@ -1,8 +1,20 @@
 
+
+# Setup things ----
 library(tidyverse)
 library(data.table)
 library(gridExtra)
+library(pbapply)
 
+ms_files <- "mzMLs" %>%
+  list.files(pattern = "Blk|Smp", full.names = TRUE) %>%
+  normalizePath()
+xdata_filled <- readRDS("XCMS/data_intermediate/current_xdata_filled.rds")
+
+
+
+# Functions ----
+pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
 grabSingleFileData <- function(filename){
   msdata <- mzR:::openMSfile(filename)
   fullhd <- mzR::header(msdata)
@@ -15,94 +27,6 @@ grabSingleFileData <- function(filename){
                         c("rt", "mz", "int"))
   return(all_data)
 }
-
-pmppm <- function(mass, ppm=4){c(mass*(1-ppm/1000000), mass*(1+ppm/1000000))}
-
-
-ms_files <- "mzMLs" %>%
-  list.files(pattern = "Blk|Smp", full.names = TRUE) %>%
-  normalizePath()
-xdata_filled <- readRDS("XCMS/data_intermediate/current_xdata_filled.rds")
-
-raw_data <- lapply(ms_files, grabSingleFileData)
-raw_data <- lapply(seq_along(raw_data), function(x){
-  cbind(fileid=basename(ms_files)[x], raw_data[[x]])
-})
-raw_data <- do.call(rbind, raw_data)
-raw_data <- raw_data[raw_data$rt>60&raw_data$rt<1100,]
-saveRDS(raw_data, file = "XCMS/data_intermediate/MS1_data_frame.rds")
-MS1_data <- readRDS("XCMS/data_intermediate/MS1_data_frame.rds")
-MS1_data$fileid <- as.character(MS1_data$fileid)
-MS1_dt <- as.data.table(MS1_data)
-rm(MS1_data)
-split_MS1_dt <- split(MS1_dt, MS1_dt$fileid)
-
-metadata <- data.frame(
-  fileid=basename(ms_files),
-  sample_group=c("Blank", "Sample")[c(1, rep(2, 24))],
-  depth=c("Blank", "DCM", "25m")[c(1, rep(c(rep(2, 3), rep(3, 3)), 4))],
-  spindir=c("Blank", "Cyclone", "Anticyclone")[c(1, rep(2, 12), rep(3, 12))],
-  time=c("Blank", "Morning", "Afternoon")[c(1, rep(c(rep(2, 6), rep(3, 6)), 2))], stringsAsFactors = FALSE
-)
-
-
-
-feature_qscores <- xdata_filled %>%
-  xcms::featureDefinitions() %>%
-  `$`("peakidx") %>%
-  sapply(FUN = function(x){
-    median(xcms::chromPeaks(xdata_filled)[x,"sn"], na.rm = TRUE)
-  }) %>%
-  data.frame(feature_num=rownames(xcms::featureDefinitions(xdata_filled)),
-             med_qscore=., stringsAsFactors = FALSE)
-
-
-feature_data <- xdata_filled %>% 
-  xcms::featureValues() %>% 
-  cbind(feature_num=rownames(.), .) %>%
-  as.data.frame(stringsAsFactors=FALSE) %>%
-  pivot_longer(cols = -feature_num, 
-               names_to = "fileid", values_to = "peak_area") %>%
-  mutate(peak_area=as.numeric(peak_area)) %>%
-  left_join(metadata %>% mutate(fileid=as.character(fileid)), by="fileid") %>%
-  filter(sample_group%in%c("Blank", "Sample")) %>%
-  left_join(xdata_filled %>% 
-              xcms::featureDefinitions() %>% 
-              as.data.frame(stringsAsFactors=FALSE) %>%
-              select(mzmin,mzmax,rtmin,rtmax) %>% 
-              cbind(feature_num=rownames(.), .) %>%
-              mutate(feature_num=as.character(feature_num)), 
-            by="feature_num") %>%
-  left_join(feature_qscores, by="feature_num")
-
-interesting <- function(feature_data_i){
-  missin_peaks <- feature_data_i %>%
-    group_by(depth) %>%
-    summarise(missin=sum(is.na(peak_area)))
-  if(any(missin_peaks$missin>c(10, 10, 1))){
-    return(NA)
-  }
-  # feature_data_i %>%
-  #   aov(formula = peak_area~depth) %>%
-  #   TukeyHSD() %>%
-  #   `$`("depth") %>%
-  #   `[`(,"p adj") %>%
-  #   `<`(0.05) %>%
-  #   any()
-  t.test(feature_data_i$peak_area[feature_data_i$depth=="DCM"],
-         feature_data_i$peak_area[feature_data_i$depth=="25m"]) %>%
-    `$`("p.value") %>%
-    `<`(0.01)
-  # boxplot(cbind(feature_data_i$peak_area[feature_data_i$depth=="DCM"],
-  #               feature_data_i$peak_area[feature_data_i$depth=="25m"]),
-  #         names=c("DCM", "25m"), main=unique(feature_data_i$feature_num))
-}
-
-feature_data$interest <- feature_data %>%
-  split(feature_data$feature_num) %>%
-  sapply(interesting) %>% rep(each=25)
-unique(feature_data$feature_num[feature_data$interest])
-
 plotPeak <- function(feature_num_i){
   feature_info <- feature_data[feature_data$feature_num==feature_num_i,]
   feature_raw <- lapply(split(feature_info, feature_info$fileid), function(row_data){
@@ -123,8 +47,126 @@ plotPeak <- function(feature_num_i){
   gp
 }
 
+
+
+# Read in raw data ----
+raw_data <- pblapply(ms_files, grabSingleFileData)
+raw_data <- pblapply(seq_along(raw_data), function(x){
+  cbind(fileid=basename(ms_files)[x], raw_data[[x]])
+}) %>% do.call(what = rbind) %>%
+  `[`(.$rt>60&.$rt<1100)
+saveRDS(raw_data, file = "XCMS/data_intermediate/MS1_data_frame.rds")
+MS1_dt <- readRDS("XCMS/data_intermediate/MS1_data_frame.rds") %>%
+  mutate(fileid=as.character(MS1_data$fileid)) %>%
+  as.data.table()
+split_MS1_dt <- split(MS1_dt, MS1_dt$fileid)
+
+metadata <- data.frame(
+  fileid=basename(ms_files),
+  sample_group=c("Blank", "Sample")[c(1, rep(2, 24))],
+  depth=c("Blank", "DCM", "25m")[c(1, rep(c(rep(2, 3), rep(3, 3)), 4))],
+  spindir=c("Blank", "Cyclone", "Anticyclone")[c(1, rep(2, 12), rep(3, 12))],
+  time=c("Blank", "Morning", "Afternoon")[c(1, rep(c(rep(2, 6), rep(3, 6)), 2))], stringsAsFactors = FALSE
+)
+
+
+
+# Calculate quality scores ----
+feature_qscores <- xdata_filled %>%
+  xcms::featureDefinitions() %>%
+  `$`("peakidx") %>%
+  sapply(FUN = function(x){
+    median(xcms::chromPeaks(xdata_filled)[x,"sn"], na.rm = TRUE)
+  }) %>%
+  data.frame(feature_num=rownames(xcms::featureDefinitions(xdata_filled)),
+             med_qscore=., stringsAsFactors = FALSE)
+
+feature_data <- xdata_filled %>% 
+  xcms::featureValues() %>% 
+  cbind(feature_num=rownames(.), .) %>%
+  as.data.frame(stringsAsFactors=FALSE) %>%
+  pivot_longer(cols = -feature_num, 
+               names_to = "fileid", values_to = "peak_area") %>%
+  mutate(peak_area=as.numeric(peak_area)) %>%
+  left_join(metadata %>% mutate(fileid=as.character(fileid)), by="fileid") %>%
+  filter(sample_group%in%c("Blank", "Sample")) %>%
+  left_join(xdata_filled %>% 
+              xcms::featureDefinitions() %>% 
+              as.data.frame(stringsAsFactors=FALSE) %>%
+              select(mzmin,mzmax,rtmin,rtmax) %>% 
+              cbind(feature_num=rownames(.), .) %>%
+              mutate(feature_num=as.character(feature_num)), 
+            by="feature_num") %>%
+  left_join(feature_qscores, by="feature_num")
+
+
+
+# Plot each chromatogram and save to pdf ----
 pdf(file = "XCMS/data_pretty/all_feature_EICs.pdf", width = 17, height = 11)
-pb <- txtProgressBar(min = 0, max = length(unique(feature_data$feature_num)))
+pb <- txtProgressBar(min = 0, max = length(unique(feature_data$feature_num)), style = 3)
+for(i in seq(1, length(unique(feature_data$feature_num)), 16)){
+  if(i+15>length(unique(feature_data$feature_num))){
+    upperlim <- length(unique(feature_data$feature_num))
+  } else {
+    upperlim <- i+15
+  }
+  gps <- lapply(sprintf("FT%03d", i:upperlim), plotPeak)
+  do.call("grid.arrange", gps)
+  setTxtProgressBar(pb, i)
+}
+close(pb)
+dev.off()
+
+
+
+# Repeat for all files and RT's ----
+ms_files <- "mzMLs" %>%
+  list.files(pattern = ".mzML", full.names = TRUE) %>%
+  setdiff(list.files(path = "mzMLs", pattern = "neg|pos", full.names = TRUE)) %>%
+  normalizePath()
+raw_data <- pblapply(ms_files, grabSingleFileData)
+raw_data <- pblapply(seq_along(raw_data), function(x){
+  cbind(fileid=basename(ms_files)[x], raw_data[[x]])
+}) %>% do.call(what = rbind) %>%
+  `[`(.$rt>60&.$rt<1100)
+saveRDS(raw_data, file = "XCMS/data_intermediate/MS1_data_frame.rds")
+MS1_dt <- readRDS("XCMS/data_intermediate/MS1_data_frame.rds") %>%
+  mutate(fileid=as.character(MS1_data$fileid)) %>%
+  as.data.table()
+split_MS1_dt <- split(MS1_dt, MS1_dt$fileid)
+metadata <- data.frame(
+  fileid=basename(ms_files),
+  sample_group=c("Blank", "QC", "Pooled", "Sample", "Standard")[
+    c(1, rep(2, 3), rep(3, 6), rep(4, 24), rep(5, 10))],
+  depth=c("Blank", "QC", "Pooled", "25m", "DCM", "Standard")[
+    c(1, rep(2, 3), rep(3, 6), rep(c(rep(4, 3), rep(5, 3)), 4), rep(6, 10))])
+feature_qscores <- xdata_filled %>%
+  xcms::featureDefinitions() %>%
+  `$`("peakidx") %>%
+  sapply(FUN = function(x){
+    median(xcms::chromPeaks(xdata_filled)[x,"sn"], na.rm = TRUE)
+  }) %>%
+  data.frame(feature_num=rownames(xcms::featureDefinitions(xdata_filled)),
+             med_qscore=., stringsAsFactors = FALSE)
+feature_data <- xdata_filled %>% 
+  xcms::featureValues() %>% 
+  cbind(feature_num=rownames(.), .) %>%
+  as.data.frame(stringsAsFactors=FALSE) %>%
+  pivot_longer(cols = -feature_num, 
+               names_to = "fileid", values_to = "peak_area") %>%
+  mutate(peak_area=as.numeric(peak_area)) %>%
+  left_join(metadata %>% mutate(fileid=as.character(fileid)), by="fileid") %>%
+  filter(sample_group%in%c("Blank", "Sample")) %>%
+  left_join(xdata_filled %>% 
+              xcms::featureDefinitions() %>% 
+              as.data.frame(stringsAsFactors=FALSE) %>%
+              select(mzmin,mzmax,rtmin,rtmax) %>% 
+              cbind(feature_num=rownames(.), .) %>%
+              mutate(feature_num=as.character(feature_num)), 
+            by="feature_num") %>%
+  left_join(feature_qscores, by="feature_num")
+pdf(file = "XCMS/data_pretty/all_feature_EICs_w_stans.pdf", width = 17, height = 11)
+pb <- txtProgressBar(min = 0, max = length(unique(feature_data$feature_num)), style = 3)
 for(i in seq(1, length(unique(feature_data$feature_num)), 16)){
   if(i+15>length(unique(feature_data$feature_num))){
     upperlim <- length(unique(feature_data$feature_num))

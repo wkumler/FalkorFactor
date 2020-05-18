87ffb8c7009a1e30530f4021c50764c849014340
wkumler <wkumler@uw.edu>
2020-05-18 16:34:02 -0700

    Added multiple comparisons test (Holm method) and functionalized

--- XCMS/scripts/treatment_diff_search.R
+++ XCMS/scripts/treatment_diff_search.R
@@ -10,49 +10,31 @@ library(tidyverse)
 final_peaks <- read.csv(file = "XCMS/data_pretty/final_peaks.csv")
 
 
+diffreport <- function(peaks, pattern_a, pattern_b){
+  split(peaks, peaks$feature) %>%
+    lapply(function(x){
+      DCM_areas <- x$M_area[grep(pattern = pattern_a, x$file_name)]
+      m25_areas <- x$M_area[grep(pattern = pattern_b, x$file_name)]
+      if(length(DCM_areas)<3|length(m25_areas)<3)return(c(0.0005, 1))
+      c(pval=t.test(DCM_areas, m25_areas)$p.value, 
+        diff=mean(DCM_areas)/mean(m25_areas))
+    }) %>% do.call(what = rbind) %>% as.data.frame() %>%
+    mutate(feature=rownames(.)) %>%
+    arrange(pval) %>%
+    mutate(p_adj=p.adjust(pval)) %>%
+    filter(p_adj<0.05) %>%
+    arrange(p_adj) %>%
+    mutate(which_enriched=c(pattern_a, pattern_b)[as.numeric(diff>1)+1]) %>%
+    split(.$which_enriched) %>%
+    lapply(function(x){x[names(x)!="which_enriched"]})
+}
+
 
 # Depth data!
-final_diffreport <- split(final_peaks, final_peaks$feature) %>%
-  lapply(function(x){
-    DCM_areas <- x$M_area[grep(pattern = "DCM", x$file_name)]
-    m25_areas <- x$M_area[grep(pattern = "25m", x$file_name)]
-    if(length(DCM_areas)<3|length(m25_areas)<3)return(c(0.0005, 1))
-    c(pval=t.test(DCM_areas, m25_areas)$p.value, 
-      diff=mean(DCM_areas)/mean(m25_areas))
-  }) %>% do.call(what = rbind) %>% as.data.frame() %>%
-  mutate(feature=rownames(.)) %>% 
-  group_by(feature) %>%
-  summarize(mzmed=median(mz), rtmed=median(rt), avgarea=mean(area)) %>%
-  arrange(mzmed)
-DCM_enriched <- final_diffreport %>%
-  filter(diff>1) %>%
-  filter(pval<0.01) %>%
-  arrange(pval)
-surface_enriched <- final_diffreport %>%
-  filter(diff<1) %>%
-  filter(pval<0.01) %>%
-  arrange(pval)
-
-
+diffreport(final_peaks, pattern_a = "DCM", pattern_b = "25m")
 
 # Diel data!
-final_diffreport <- split(final_peaks, final_peaks$feature) %>%
-  lapply(function(x){
-    AM_areas <- x$M_area[grep(pattern = "62|77", x$file_name)]
-    PM_areas <- x$M_area[grep(pattern = "64|80", x$file_name)]
-    if(length(AM_areas)<3|length(PM_areas)<3)return(c(0.0005, 1))
-    c(pval=t.test(AM_areas, PM_areas)$p.value, 
-      diff=mean(AM_areas)/mean(PM_areas))
-  }) %>% do.call(what = rbind) %>% as.data.frame() %>%
-  mutate(feature=rownames(.)) %>% 
-  group_by(feature) %>%
-  summarize(mzmed=median(mz), rtmed=median(rt), avgarea=mean(area)) %>%
-  arrange(mzmed)
-AM_enriched <- final_diffreport %>%
-  filter(diff>1) %>%
-  filter(pval<0.01) %>%
-  arrange(pval)
-PM_enriched <- final_diffreport %>%
-  filter(diff<1) %>%
-  filter(pval<0.01) %>%
-  arrange(pval)
+diffreport(final_peaks, pattern_a = "62|77", pattern_b = "64|80")
+
+# Direction data!
+diffreport(final_peaks, pattern_a = "62|64", pattern_b = "77|80")

5ba3fb0ccf86968da6973e45226d9d37eb0d4152
wkumler <wkumler@uw.edu>
2020-06-01 11:51:43 -0700

    Giving up on Regina's B-MIS code

--- Targeted_Pipeline/src/MSDial_Import.R
+++
@@ -1,8 +0,0 @@
-
-# Import all files --------------------------------------------------
-filenames <- RemoveCsv(list.files(path = 'data_raw', pattern = file.pattern))
-
-for (i in filenames) {
-  filepath <- file.path('data_raw', paste(i,".csv", sep = ""))
-  assign(i, read.csv(filepath, stringsAsFactors = FALSE))
-}

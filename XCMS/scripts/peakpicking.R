6868f7afdff06f78d678604119087a015db54f87
wkumler <wkumler@uw.edu>
2020-06-16 01:07:09 -0700

    Debugging new XCMS version during rerun

--- XCMS/scripts/peakpicking.R
+++ XCMS/scripts/peakpicking.R
@@ -185,6 +185,191 @@ findIsoAdduct <- function(file_peaks, xdata, grabSingleFileData,
   iso_matches_all <- do.call(rbind, iso_matches_all)
   return(cbind(file_peaks, iso_matches_all))
 }
+fillChromPeaks_wkumler <- function(object, param){
+  msLevel <- 1L
+  BPPARAM <- bpparam()
+  
+  if (length(msLevel) != 1) 
+    stop("Can only perform peak filling for one MS level at a time")
+  if (!hasFeatures(object, msLevel = msLevel)) 
+    stop("No feature definitions for MS level ", 
+         msLevel, " present. Please run 'groupChromPeaks' first.")
+  if (xcms:::.hasFilledPeaks(object)) 
+    message("Filled peaks already present, adding still missing", 
+            " peaks.")
+  if (xcms:::hasChromPeaks(object) & !xcms:::.has_chrom_peak_data(object)) 
+    object <- updateObject(object)
+  startDate <- date()
+  expandMz <- expandMz(param)
+  expandRt <- expandRt(param)
+  fixedMz <- fixedMz(param)
+  fixedRt <- fixedRt(param)
+  ppm <- ppm(param)
+  message("Defining peak areas for filling-in .", 
+          appendLF = FALSE)
+  fdef <- featureDefinitions(object, msLevel = msLevel)
+  aggFunLow <- median
+  aggFunHigh <- median
+  tmp_pks <- chromPeaks(object)[, c("rtmin", "rtmax", 
+                                    "mzmin", "mzmax")]
+  pkArea <- do.call(rbind, lapply(fdef$peakidx, function(z) {
+    pa <- c(aggFunLow(tmp_pks[z, 1]), aggFunHigh(tmp_pks[z, 2]), 
+            aggFunLow(tmp_pks[z, 3]), aggFunHigh(tmp_pks[z, 4]))
+    if (ppm != 0) {
+      mzmean <- mean(pa[3:4])
+      tittle <- mzmean * (ppm/2)/1e+06
+      if ((pa[4] - pa[3]) < (tittle * 2)) {
+        pa[3] <- mzmean - tittle
+        pa[4] <- mzmean + tittle
+      }
+    }
+    if (expandRt != 0) {
+      diffRt <- (pa[2] - pa[1]) * expandRt/2
+      pa[1] <- pa[1] - diffRt
+      pa[2] <- pa[2] + diffRt
+    }
+    if (expandMz != 0) {
+      diffMz <- (pa[4] - pa[3]) * expandMz/2
+      pa[3] <- pa[3] - diffMz
+      pa[4] <- pa[4] + diffMz
+    }
+    if (fixedMz != 0) {
+      pa[3] <- pa[3] - fixedMz
+      pa[4] <- pa[4] + fixedMz
+    }
+    if (fixedRt != 0) {
+      pa[1] <- pa[1] - fixedRt
+      pa[2] <- pa[2] + fixedRt
+    }
+    pa
+  }))
+  rm(tmp_pks)
+  message(".", appendLF = FALSE)
+  colnames(pkArea) <- c("rtmin", "rtmax", "mzmin", "mzmax")
+  pkArea <- cbind(group_idx = 1:nrow(pkArea), pkArea, mzmed = as.numeric(fdef$mzmed))
+  pkGrpVal <- featureValues(object, value = "index", msLevel = msLevel)
+  message(".", appendLF = FALSE)
+  if (!any(is.na(rowSums(pkGrpVal)))) {
+    message("No missing peaks present.")
+    return(object)
+  }
+  message(".", appendLF = FALSE)
+  objectL <- vector("list", length(fileNames(object)))
+  pkAreaL <- objectL
+  req_fcol <- requiredFvarLabels("OnDiskMSnExp")
+  min_fdata <- fData(object)[, req_fcol]
+  rt_range <- range(pkArea[, c("rtmin", "rtmax")])
+  if (hasAdjustedRtime(object)) 
+    min_fdata$retentionTime <- adjustedRtime(object)
+  for (i in 1:length(fileNames(object))) {
+    fd <- min_fdata[min_fdata$fileIdx == i, ]
+    fd$fileIdx <- 1L
+    objectL[[i]] <- new("OnDiskMSnExp", 
+                        processingData = new("MSnProcess", 
+                                             files = fileNames(object)[i]), 
+                        featureData = new("AnnotatedDataFrame", fd), 
+                        phenoData = new("NAnnotatedDataFrame", 
+                                        data.frame(sampleNames = "1")), 
+                        experimentData = new("MIAPE", 
+                                             instrumentManufacturer = "a", 
+                                             instrumentModel = "a", 
+                                             ionSource = "a", 
+                                             analyser = "a", 
+                                             detectorType = "a"))
+    pkAreaL[[i]] <- pkArea[is.na(pkGrpVal[, i]), , drop = FALSE]
+  }
+  rm(pkGrpVal)
+  rm(pkArea)
+  rm(min_fdata)
+  message(" OK\nStart integrating peak areas from original files")
+  ph <- processHistory(object, type = xcms:::.PROCSTEP.PEAK.DETECTION)
+  findPeakMethod <- "unknown"
+  mzCenterFun <- "wMean"
+  if (length(ph)) {
+    if (is(ph[[1]], "XProcessHistory")) {
+      prm <- ph[[1]]@param
+      findPeakMethod <- .param2string(prm)
+      if (.hasSlot(prm, "mzCenterFun")) 
+        mzCenterFun <- prm@mzCenterFun
+    }
+  }
+  cp_colnames <- colnames(chromPeaks(object))
+  mzCenterFun <- paste("mzCenter", gsub(mzCenterFun, 
+                                        pattern = "mzCenter.", replacement = "", 
+                                        fixed = TRUE), sep = ".")
+  if (findPeakMethod == "MSW") {
+    rts <- rtime(object, bySample = TRUE)
+    if (any(lengths(rts) > 1)) 
+      stop("The data is supposed to be direct injection data, ", 
+           "but I got files with more than one spectrum/", 
+           "retention time!")
+    res <- bpmapply(FUN = xcms:::.getMSWPeakData, objectL, pkAreaL, 
+                    as.list(1:length(objectL)), MoreArgs = list(cn = cp_colnames), 
+                    BPPARAM = BPPARAM, SIMPLIFY = FALSE)
+  } else if (findPeakMethod == "matchedFilter") {
+    res <- bpmapply(FUN = xcms:::.getChromPeakData_matchedFilter, 
+                    objectL, pkAreaL, as.list(1:length(objectL)), 
+                    MoreArgs = list(cn = cp_colnames, param = prm, 
+                                    msLevel = msLevel), 
+                    BPPARAM = BPPARAM, 
+                    SIMPLIFY = FALSE)
+  } else {
+    res <- bpmapply(FUN = xcms:::.getChromPeakData, objectL, 
+                    pkAreaL, as.list(1:length(objectL)), 
+                    MoreArgs = list(cn = cp_colnames, mzCenterFun = mzCenterFun, 
+                                    msLevel = msLevel), 
+                    BPPARAM = BPPARAM, SIMPLIFY = FALSE)
+  }
+  rm(objectL)
+  res <- do.call(rbind, res)
+  res <- cbind(res, group_idx = unlist(lapply(pkAreaL, function(z){
+    z[, "group_idx"]}), use.names = FALSE))
+  res <- res[!is.na(res[, "into"]), , drop = FALSE]
+  if (nrow(res) == 0) {
+    warning("Could not integrate any signal for the missing ", 
+            "peaks! Consider increasing 'expandMz' and 'expandRt'.")
+    return(object)
+  }
+  rm(pkAreaL)
+  gc()
+  newFd <- new("MsFeatureData")
+  newFd@.xData <- xcms:::.copy_env(object@msFeatureData)
+  object@msFeatureData <- new("MsFeatureData")
+  incr <- nrow(chromPeaks(newFd))
+  for (i in unique(res[, "group_idx"])) {
+    fdef$peakidx[[i]] <- c(fdef$peakidx[[i]], (which(res[, "group_idx"] == i) + incr))
+  }
+  fdef <- rbind(fdef, featureDefinitions(newFd)[featureDefinitions(newFd)$ms_level != 
+                                                  msLevel, , drop = FALSE])
+  if (!any(colnames(fdef) == "ms_level")) {
+    fdef$ms_level <- 1L
+  } else {fdef <- fdef[order(fdef$ms_level), ]}
+  maxId <- max(as.numeric(gsub("^.*\\.", "", rownames(chromPeaks(newFd)))))
+  if (maxId < 1)
+    stop("chromPeaks matrix lacks rownames; please update ", 
+         "'object' with the 'updateObject' function.")
+  toId <- maxId + nrow(res)
+  rownames(res) <- sprintf(paste0("CP", "%0", ceiling(log10(toId + 1L)), "d"), 
+                           (maxId + 1L):toId)
+  chromPeaks(newFd) <- rbind(chromPeaks(newFd), res[, -ncol(res)])
+  cpd <- chromPeakData(newFd)[rep(1L, nrow(res)), , drop = FALSE]
+  cpd[, ] <- NA
+  cpd$ms_level <- as.integer(msLevel)
+  cpd$is_filled <- TRUE
+  if (!any(colnames(chromPeakData(newFd)) == "is_filled")) 
+    chromPeakData(newFd)$is_filled <- FALSE
+  chromPeakData(newFd) <- rbind(chromPeakData(newFd), cpd)
+  rownames(chromPeakData(newFd)) <- rownames(chromPeaks(newFd))
+  featureDefinitions(newFd) <- fdef
+  lockEnvironment(newFd, bindings = TRUE)
+  object@msFeatureData <- newFd
+  ph <- xcms:::XProcessHistory(param = param, date. = startDate, 
+                        type. = xcms:::.PROCSTEP.PEAK.FILLING, 
+                        fileIndex = 1:length(fileNames(object)), 
+                        msLevel = msLevel)
+  object <- xcms:::addProcessHistory(object, ph)
+  object
+}
 
 
 
@@ -223,7 +408,7 @@ register(BPPARAM = SnowParam(tasks = length(ms_files), progressbar = TRUE))
 # Peakpicking ----
 raw_data <- readMSData(files = ms_files, msLevel. = 1, 
                        verbose = TRUE, centroided. = TRUE, mode = "onDisk",
-                       pdata = as(metadata, "AnnotatedDataFrame"))
+                       pdata = as(metadframe, "AnnotatedDataFrame"))
 cwp <- CentWaveParam(ppm = 2.5, peakwidth = c(15, 15), 
                      snthresh = 1, prefilter = c(0, 10000), 
                      integrate = 2, mzCenterFun = "wMean", 
@@ -262,13 +447,13 @@ message(Sys.time()-start_time)
 
 
 # Other XCMS things (rtcor, group) ----
-register(BPPARAM = SerialParam())
+xdata_cleanpeak <- readRDS("XCMS/data_intermediate/xdata_cleanpeak.rds")
 register(BPPARAM = SnowParam(tasks = length(ms_files), progressbar = TRUE))
 obp <- ObiwarpParam(binSize = 0.1, centerSample = 4, 
                     response = 1, distFun = "cor_opt")
 xdata_rt <- suppressMessages(adjustRtime(xdata_cleanpeak, param = obp))
 plotAdjustedRtime(xdata_rt, col = c("green", "red", "blue", "black", "black")[
-  factor(metadata@data$depth)])
+  factor(metadframe$depth)])
 
 pdp <- PeakDensityParam(sampleGroups = xdata_rt$depth, 
                         bw = 5, minFraction = 0.5, 
@@ -276,7 +461,7 @@ pdp <- PeakDensityParam(sampleGroups = xdata_rt$depth,
 xdata_cor <- groupChromPeaks(xdata_rt, param = pdp)
 
 fpp <- FillChromPeaksParam()
-xdata_filled <- suppressMessages(fillChromPeaks(xdata_cor, param = fpp))
+xdata_filled <- suppressMessages(fillChromPeaks_wkumler(xdata_cor, param = fpp))
 
 saveRDS(xdata_filled, file = "XCMS/data_intermediate/current_xdata_filled.rds")
 message(Sys.time()-start_time)

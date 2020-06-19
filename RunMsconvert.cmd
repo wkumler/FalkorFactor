5d5b4650f4f7de1e97a4a7fbedc7f883e66f4df8
wkumler <wkumler@uw.edu>
2020-06-19 00:44:38 -0700

    Using new mzMLs - adding Neg mode and including new blanks from Katherine

--- RunMsconvert.cmd
+++ RunMsconvert.cmd
@@ -1,3 +1,6 @@
-msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs/
-msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o falkor_mzMLs/
-msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\MSMS\*.raw --mzML --filter "peakPicking true 1-" -z -o mzMLs\MSMS\
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs_pos/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs_pos/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\MSMS\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs_pos\MSMS\
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity negative" -z -o mzMLs_neg/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity negative" -z -o mzMLs_neg/
+msconvert Z:\1_QEdata\LTC\DATA\HILIC\190718_DepthProfiles_FK180310\MSMS\*.raw --mzML --filter "peakPicking true 1-" --filter "polarity negative" -z -o mzMLs_neg\MSMS\

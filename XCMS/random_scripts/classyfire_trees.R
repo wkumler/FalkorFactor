779bb3bfbc5ef0d9f5f4d5073f938df436ad33d8
wkumler <wkumler@uw.edu>
2020-07-12 19:09:43 -0700

    Adding comments before sending to Leland

--- XCMS/random_scripts/classyfire_trees.R
+++ XCMS/random_scripts/classyfire_trees.R
@@ -4,10 +4,15 @@ library(tidygraph)
 
 
 # Build tree ----
-raw_json <- jsonlite::fromJSON("http://classyfire.wishartlab.com/tax_nodes.json")
-raw_json <- raw_json[-1,c(2,3,1)]
-names(raw_json) <- c("id", "parent", "class")
-head(raw_json)
+# Download raw data in JSON format
+classyclasses <- jsonlite::fromJSON("http://classyfire.wishartlab.com/tax_nodes.json")
+# Drop useless columns
+classyclasses <- classyclasses[-1,c(2,3,1)]
+# Rename to avoid data.tree naming problems
+names(classyclasses) <- c("id", "parent", "class")
+head(classyclasses)
+# Save to disk
+write.csv(classyclasses, "classyclasses.csv")
 
 sirius_output <- "XCMS/data_intermediate/sirius_project/output_dir/" %>%
   paste0("canopus_summary.tsv") %>%
@@ -27,8 +32,8 @@ classes_found <- sirius_output$`all classifications` %>%
   data.frame() %>%
   `names<-`(c("class", "count"))
 
-compound_count <- merge(raw_json, classes_found, by="class")
-classyfire_tree <- FromDataFrameNetwork(raw_json)
+compound_count <- merge(classyclasses, classes_found, by="class")
+classyfire_tree <- FromDataFrameNetwork(classyclasses)
 print(classyfire_tree, "class", "count")
 
 

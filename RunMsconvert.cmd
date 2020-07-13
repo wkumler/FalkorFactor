4c8ceff751d7be562915c006ee97713c4fc2ea86
wkumler <wkumler@uw.edu>
2020-07-13 12:36:02 -0700

    Fixed MSMS file path

--- RunMsconvert.cmd
+++ RunMsconvert.cmd
@@ -11,4 +11,4 @@ msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2018\180205_Wei_MESO-SCOPE_HRM_DepthP
 msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity negative" -z -o mzMLs/neg/
 msconvert Z:\1_QEdata\LTC\DATA\HILIC\HILIC_2017\170706_Gradients2.0_CruiseFilters_2\170706_Blk_Blk0p2_?.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs/pos/
 
-msconvert Z:\1_QEdata\Will\falkor_MSMS_inclusion\*DDApos*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs/pos/
+msconvert Z:\1_QEdata\Will\falkor_MSMS_inclusion\*DDApos*.raw --mzML --filter "peakPicking true 1-" --filter "polarity positive" -z -o mzMLs/pos/MSMS/

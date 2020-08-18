fe431ed2b80fdc33816e7abd3fb7b7794f9e5e66
wkumler <wkumler@uw.edu>
2020-08-18 13:42:17 -0700

    Trying to deal with isomer standards

--- XCMS/scripts/standard_assignments.R
+++ XCMS/scripts/standard_assignments.R
@@ -1,13 +1,31 @@
 
+stan_data <- all_stans[3,]
+stan_data <- all_stans %>% filter(mz%between%pmppm(118.0865)) %>% slice(1)
+
+duplicate_masses <- all_stans %>% 
+  filter(duplicated(mz)|duplicated(mz, fromLast=TRUE)) %>%
+  arrange(as.numeric(mz), as.numeric(rt)) %>%
+  select(compound_name, rt, mz)
+
 
 apply(all_stans, 1, function(stan_data){
   possible_stan_features <- final_features %>%
     filter(mzmed%between%pmppm(as.numeric(stan_data["mz"])))
-  final_peaks %>%
+  stan_vals <- final_peaks %>%
     filter(feature%in%possible_stan_features$feature) %>%
     select(feature, mz, rt, into, sn, file_name, M_area) %>%
-    group_by(feature) %>%
     filter(grepl("Std", .$file_name)) %>%
-    mutate(stan_mix=str_extract(file_name, "4uMStdsMix[1|2]|H2OinMatrix")) %>%
-    summarize(p_anova=summary(aov(formula = M_area~stan_mix, data = .))[[1]]$`Pr(>F)`[1])
+    mutate(stan_mix=str_extract(file_name, "4uMStdsMix[1|2]|H2OinMatrix"))
+    
+  if(length(unique(stan_vals$stan_mix))<2){
+    return(0)
+  } else {
+    stan_vals %>%
+      split(.$feature) %>%
+      lapply(aov, formula=M_area~stan_mix) %>%
+      lapply(summary) %>%
+      lapply(`[[`, 1) %>%
+      lapply(`[[`, "Pr(>F)") %>%
+      lapply(`[`, 1)
+  }
 })

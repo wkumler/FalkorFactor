11ec897086f53d435591dccdf7ca9ef61b8c35fc
wkumler <wkumler@uw.edu>
2020-08-24 13:20:14 -0700

    Squashed bug from variable rename

--- XCMS/scripts/deisoadduct.R
+++ XCMS/scripts/deisoadduct.R
@@ -52,7 +52,7 @@ safe_features <- sapply(not_addisos, function(safe_data){
            rt%between%(safe_data["rt"]+c(-50, 50))) %>%
     pull(feature) %>% unique()
 })
-likely_addisos <- filter(likely_addisos, !feature%in%safe_peaks)
+likely_addisos <- filter(likely_addisos, !feature%in%safe_features)
 
 
 addiso_peaks <- raw_peaks %>%

5b06c8a37626833aa1db217d49defa591ce7d398
wkumler <wkumler@uw.edu>
2020-09-04 11:45:54 -0700

    Improved stan assignments with two-factor anova to catch betaine

--- XCMS/scripts/standard_assignments.R
+++ XCMS/scripts/standard_assignments.R
@@ -4,233 +4,67 @@ duplicate_masses <- all_stans %>%
   arrange(as.numeric(mz), as.numeric(rt)) %>%
   select(compound_name, rt, mz)
 
-
-stan_assignments <- pbapply(all_stans, 1, function(stan_data){
-  possible_stan_features <- final_features %>%
-    filter(mzmed%between%pmppm(as.numeric(stan_data["mz"])))
-  stan_vals <- final_peaks %>%
-    filter(feature%in%possible_stan_features$feature) %>%
-    select(feature, mz, rt, into, sn, file_name, M_area) %>%
-    filter(grepl("Std", .$file_name)) %>%
-    mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-    split(.$feature)
-  
-  stan_diffs <- sapply(stan_vals, function(x){
-    if(sum(x$correct_mix)<2|sum(!x$correct_mix)<2){
-      return(0)
+stan_assignments <- all_stans %>%
+  split(.$compound_name) %>%
+  pblapply(function(stan_data){
+    dput(stan_data)
+    possible_stan_features <- all_features %>%
+      filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
+      arrange(rtmed)
+    
+    # If the compound has an isotopologue, use RT matching
+    isotopologue <- stan_data$compound_name %>%
+      paste0(", [15N|13C|2H]") %>%
+      grep(x = all_stans$compound_name, value = TRUE)
+    if(length(isotopologue)==1){
+      isotopo_data <- all_stans %>% 
+        filter(compound_name==isotopologue)
+      isotopo_features <- all_features %>%
+        filter(mzmed%between%pmppm(isotopo_data$mz, 5))
+      if(nrow(isotopo_features)==1){
+        isotope_choice <- possible_stan_features %>% 
+          mutate(rtdiff=abs(rtmed-isotopo_features$rtmed)) %>%
+          arrange(desc(rtdiff)) %>%
+          slice(1) %>%
+          pull(feature)
+      } else {
+        isotope_choice <- NA
+      }
     } else {
-      return(t.test(M_area~correct_mix, data = x)$p.value)
+      isotope_choice <- NA
     }
+    
+    # If there's a peak that differs between the mixes
+    if(is.na(stan_data$mix)){
+      mix_choice <- NA
+    } else {
+      stan_pvals <- real_peaks %>%
+        filter(feature%in%possible_stan_features$feature) %>%
+        select(feature, mz, rt, into, file_name, M_area) %>%
+        filter(grepl("Std", .$file_name)) %>%
+        mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
+        mutate(stan_type=str_extract(file_name, "InH2O|InMatrix|H2OinMatrix")) %>%
+        split(.$feature) %>%
+        sapply(function(x){
+          if(sum(x$correct_mix)<2|sum(!x$correct_mix)<2){
+            return(0)
+          } else {
+            aov_res <- summary(aov(x, formula = M_area~correct_mix+stan_type))
+            pvals <- aov_res[[1]]$`Pr(>F)`
+            names(pvals) <- c("correct_mix", "stan_type", "Residuals")
+            pvals
+          }
+        }, simplify = FALSE)
+      mix_choice <- names(stan_pvals)[
+        which(sapply(stan_pvals, function(x)x["correct_mix"]<0.05&x["stan_type"]>0.05))
+      ]
+    }
+
+    # If there's one peak much closer in RT to expected than the others
+    expected_rt <- stan_data$rt*60
+    rt_choice <- possible_stan_features %>% 
+      mutate(rtdiff=abs(rtmed-expected_rt)) %>%
+      arrange(rtdiff) %>%
+      slice(1) %>%
+      pull(feature)
   })
-})
-
-all_stans[which(sapply(stan_assignments, length)==5),]
-
-
-
-# Glycine (5 possible peaks)
-stan_row <- 47
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"])))
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/76.03994.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 500000)) +
-  xlim(c(100, 750)) + 
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-
-
-
-all_stans[which(sapply(stan_assignments, length)==4),]
-
-# Allopurinol, hypoxanthine (4 possible peaks)
-all_stans[c(91, 106),]
-stan_row <- 91
-stan_data <- all_stans[stan_row,]
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"])))
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/137.046336.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  facet_wrap(~fileid) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 2000000)) +
-  xlim(200, 500) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-# Betonicine/turicine
-all_stans[c(9, 85),]
-stan_row <- 9
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"])))
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/160.0969.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 1000000)) +
-  xlim(300, 600) +
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-
-# Leucine, isoleucine, TMAP
-all_stans[c(55, 56, 98),]
-stan_row <- 55
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rt)
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/132.102.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  xlim(300, 600) + 
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 10000000)) +
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-
-
-
-all_stans[which(sapply(stan_assignments, length)==3),]
-
-# Sarcosine, alanine, beta-alanine
-all_stans[c(18, 69, 93),]
-stan_row <- 18
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rt)
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/90.05551.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  xlim(500, 700) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 10000000)) +
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-# Cytosine
-all_stans[36,]
-stan_row <- 36
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rt)
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/112.051087.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  xlim(300, 500) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 10000000)) +
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
-
-# Proline
-all_stans[66,]
-stan_row <- 66
-stan_data <- all_stans[stan_row,]
-filter(all_stans, mz%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rt)
-possible_stan_features <- final_features %>%
-  filter(mzmed%between%pmppm(as.numeric(stan_data["mz"]))) %>%
-  arrange(rtmed)
-stan_vals <- final_peaks %>%
-  filter(feature%in%possible_stan_features$feature) %>%
-  select(feature, mz, rt, into, sn, file_name, M_area) %>%
-  filter(grepl("Std", .$file_name)) %>%
-  mutate(correct_mix=grepl(file_name, pattern = stan_data["mix"])) %>%
-  split(.$feature)
-stan_assignments[[stan_row]] %>% data.frame(feature=names(.), pval=.) %>% 
-  left_join(possible_stan_features) %>% arrange(rtmed)
-read.csv("C:/Users/willi/Downloads/116.071154.csv") %>%
-  mutate(fillcol=str_extract(fileid, "Blk|Smp|Std")) %>%
-  filter(fillcol=="Std") %>%
-  mutate(stdtype=str_extract(fileid, "Mix[1|2]|H2O")) %>%
-  ggplot() + 
-  geom_polygon(aes(x=rt, y=int, fill=stdtype)) +
-  geom_line(aes(x=rt, y=int, group=stdtype)) +
-  scale_y_continuous(oob = scales::rescale_none, limits = c(0, 1000000)) +
-  facet_wrap(~fileid) +
-  scale_fill_viridis_d(alpha = 0.7, option = "C")
